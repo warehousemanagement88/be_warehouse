26fcbfec809e2bb7913eadf5de3f643f02229de7
Agitanurfd <agitanurfadillah45@gmail.com>
2023-10-29 18:18:12 +0700

    menambahkan struct

--- model/type.go
+++ model/type.go
@@ -5,21 +5,21 @@ import (
 )
 
 type User struct {
-	ID           	primitive.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
-	FirstName  		string             `bson:"firstname,omitempty" json:"firstname,omitempty"`
-	LastName  		string             `bson:"lastname,omitempty" json:"lastname,omitempty"`
-	Email  			string             `bson:"email,omitempty" json:"email,omitempty"`
-	Password        string         	   `bson:"password,omitempty" json:"password,omitempty"`
-	Confirmpassword string         	   `bson:"confirmpass,omitempty" json:"confirmpass,omitempty"`
-	Salt 			string			   `bson:"salt,omitempty" json:"salt,omitempty"`
-	Role     		string			   `bson:"role,omitempty" json:"role,omitempty" `
+	ID              primitive.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
+	FirstName       string             `bson:"firstname,omitempty" json:"firstname,omitempty"`
+	LastName        string             `bson:"lastname,omitempty" json:"lastname,omitempty"`
+	Email           string             `bson:"email,omitempty" json:"email,omitempty"`
+	Password        string             `bson:"password,omitempty" json:"password,omitempty"`
+	Confirmpassword string             `bson:"confirmpass,omitempty" json:"confirmpass,omitempty"`
+	Salt            string             `bson:"salt,omitempty" json:"salt,omitempty"`
+	Role            string             `bson:"role,omitempty" json:"role,omitempty" `
 }
 
 type Item struct {
-	ID              		primitive.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
-	Name            		string			   `bson:"name,omitempty" json:"name,omitempty"`
-	Quantity         		string			       `bson:"quantity,omitempty" json:"quantity,omitempty"`
-	Category     		    string			   `bson:"category,omitempty" json:"category,omitempty"`
+	ID       primitive.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
+	Name     string             `bson:"name,omitempty" json:"name,omitempty"`
+	Quantity string             `bson:"quantity,omitempty" json:"quantity,omitempty"`
+	Category string             `bson:"category,omitempty" json:"category,omitempty"`
 }
 
 type Credential struct {
