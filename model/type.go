76ecba5e59a3f4301aff242780752aaa1cb3cf15
Agitanurfd <agitanurfadillah45@gmail.com>
2023-11-26 17:18:50 +0700

    menambahkan struct password

--- model/type.go
+++ model/type.go
@@ -18,9 +18,10 @@ type User struct {
 }
 
 type Password struct {
-	Password			string				`bson:"password,omitempty" json:"password,omitempty"`
-	Newpassword			string				`bson:"newpass,omitempty" json:"newpass,omitempty"`
-	Confirmpassword		string				`bson:"confirmpass,omitempty" json:"confirmpass,omitempty"`
+	Password         string `bson:"password,omitempty" json:"password,omitempty"`
+	Newpassword      string `bson:"newpass,omitempty" json:"newpass,omitempty"`
+	Confirmpassword  string `bson:"confirmpass,omitempty" json:"confirmpass,omitempty"`
+	Confirmpasswordd string `bson:"confirmpasss,omitempty" json:"confirmpass,omitempty"`
 }
 
 type Staff struct {
